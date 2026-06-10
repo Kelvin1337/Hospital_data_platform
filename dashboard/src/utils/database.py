1ff71791df27e78e55bada6ee8f1d3381c5039b7
Kelvin <kelvindasilva_2022@hotmail.com>
2026-06-10 15:28:06 -0300

    atualização foto do projeto

--- dashboard/src/utils/database.py
+++ dashboard/src/utils/database.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import snowflake.connector
 import pandas as pd
-
 def get_connection():
     return snowflake.connector.connect(
-        user=st.secrets["user"],
-        password=st.secrets["password"],
-        account=st.secrets["account"],
-        warehouse=st.secrets["warehouse"],
-        database=st.secrets["database"],
-        schema=st.secrets["schema"]
+        user=st.secrets["snowflake"]["user"],
+        password=st.secrets["snowflake"]["password"],
+        account=st.secrets["snowflake"]["account"],
+        warehouse=st.secrets["snowflake"]["warehouse"],
+        database=st.secrets["snowflake"]["database"],
+        schema=st.secrets["snowflake"]["schema"],
+        role=st.secrets["snowflake"]["role"]
     )
 @st.cache_data(ttl=3600) # Guarda o resultado por 1 hora na memória do Streamlit
 def run_query(query):
